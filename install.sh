e0d41ccb99bcbb1970956f2e34d2abe164f1db36
zengtong <unixsudo@outlook.com>
2019-05-06 22:54:14 +0800

    simplify install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
-path=$(cd `dirname $0`;pwd)
+#path=$(cd `dirname $0`;pwd)
 read -p "input mysql command:" command
 read -p "set query alias:" query
 #save mysql command
-echo "$command" > .$query.sh
-#query script
-query_sh="val=\$(echo \"\$1\" | source $path/.$query.sh)\necho \"\$val\""
-echo "$query_sh" > $query
+echo "echo \"\$1\" | $command" > $query
 chmod +x $query
